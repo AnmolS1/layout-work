be9ad0626314436c60766ea306ec301bd555e925
Anmol Saxena <anmolsaxena456@gmail.com>
2023-05-03 07:42:14 -0500

    wanna see what vercel sees

--- src/pages/Validator.js
+++ src/pages/Validator.js
@@ -29,7 +29,10 @@ const Validator = () => {
 		return new Promise((resolve, reject) => {
 			const fileReader = new FileReader();
 			
-			fileReader.onload = event => resolve(JSON.parse(event.target.result));
+			fileReader.onload = event => {
+				console.log(event);
+				resolve(JSON.parse(event.target.result))
+			};
 			fileReader.onerror = error => reject(error);
 			
 			fileReader.readAsText(file);
