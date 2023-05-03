4ed0c658d3f1b3cdd6089b692dc74dacda0dca56
Anmol Saxena <anmolsaxena456@gmail.com>
2023-05-03 07:32:30 -0500

    testing vercel

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,74 +1,76 @@
 import { css } from 'styled-components/macro';
 
+const public_url = process.env.PUBLIC_URL;
+
 const fonts = css`
 	@font-face {
 		font-family: 'Plex Bold';
-		src: url('../fonts/IBMPlexSans-Bold.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Bold.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Bold Italic';
-		src: url('../fonts/IBMPlexSans-BoldItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-BoldItalic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Extra Light';
-		src: url('../fonts/IBMPlexSans-ExtraLight.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-ExtraLight.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Extra Light Italic';
-		src: url('../fonts/IBMPlexSans-ExtraLightItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-ExtraLightItalic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Italic';
-		src: url('../fonts/IBMPlexSans-Italic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Italic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Light';
-		src: url('../fonts/IBMPlexSans-Light.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Light.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Light Italic';
-		src: url('../fonts/IBMPlexSans-LightItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-LightItalic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Medium';
-		src: url('../fonts/IBMPlexSans-Medium.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Medium.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Medium Italic';
-		src: url('../fonts/IBMPlexSans-MediumItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-MediumItalic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex';
-		src: url('../fonts/IBMPlexSans-Regular.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Regular.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex SemiBold';
-		src: url('../fonts/IBMPlexSans-SemiBold.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-SemiBold.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex SemiBold Italic';
-		src: url('../fonts/IBMPlexSans-SemiBoldItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-SemiBoldItalic.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Thin';
-		src: url('../fonts/IBMPlexSans-Thin.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-Thin.ttf') format('truetype');
 	}
 	
 	@font-face {
 		font-family: 'Plex Thin Italic';
-		src: url('../fonts/IBMPlexSans-ThinItalic.ttf') format('truetype');
+		src: url('${public_url}/fonts/IBMPlexSans-ThinItalic.ttf') format('truetype');
 	}
 `;
 
